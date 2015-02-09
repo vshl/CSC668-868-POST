5bfa3a8fc73feb5180df08861e0a3cb8189346d7
Ryan Eshleman <ryaneshleman@gmail.com>
2015-02-08 20:21:01 -0800

    added stub methods to POST an dSale classes

--- POST/src/post/POST.java
+++ POST/src/post/POST.java
@@ -4,16 +4,81 @@
  */
 package post;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author ryaneshleman
  */
 public class POST {
-
+    private final String PRODUCT_DESC_FILE = "./products.txt";
+    private final String TRANSACTIONS_FILE = "./transactions.txt";
+    private final String SALES_LOG_FILE = "./sales_log.txt";
+    
+    private Catalog    catalog;
+    private List<Sale> sales = new ArrayList<Sale>();
+    
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
     }
+    
+    /**
+     * initializes the POST, builds 
+     */
+    public boolean init()
+    {
+        //TODO
+    }
+    
+    
+    public boolean shutDown()
+    {
+        //TODO
+    }
+    
+    /**
+     * adds item to catalog
+     * @param itemDesc
+     * @return 
+     */
+    public ItemDescription addToCatalog(ItemDescription itemDesc)
+    {
+        //TODO
+    }
+    
+    /**
+     * removes item from catalog
+     * @param upc
+     * @return 
+     */
+    public ItemDescription removeFromCatalog(int upc)
+    {
+        //TODO
+    }  
+    
+    /**
+     * if the sale is completed ie s.isCompleted == true, the sale is written
+     * in the sales log
+     * @param s
+     * @return 
+     */
+    public boolean recordSale(Sale s)
+    {
+        //TODO
+    }
+    
+    /**
+     * returns the contents of the sales log 
+     * @return 
+     */
+    public String salesLog()
+    {
+        //TODO
+    }        
+    
 }
--- POST/src/post/Sale.java
+++ POST/src/post/Sale.java
@@ -4,10 +4,50 @@
  */
 package post;
 
+import java.util.List;
+
 /**
  *
  * @author ryaneshleman
  */
 public class Sale {
+    private double  amountDue = 0;
+    private boolean isComplete = false;
+    private int     numItems = 0;
     
+    private Catalog catalog;
+    
+    
+    public Sale(Catalog catalog)
+    {
+        this.catalog = catalog;
+        //TODO
+    }        
+    
+    public boolean initiateSale(String custName, List<Integer> upcList)
+    {
+        //TODO
+    }
+    
+    public ItemDescription addItem(int upc)
+    {
+        //TODO
+    }
+    
+    public boolean makeCreditCardPayment(int cardNumber)
+    {
+        //TODO
+    }
+    
+    public double makeCashPayment(double amount)
+    {
+        //TODO
+    }
+    
+    public String generateReceipt()
+    {
+        //TODO
+    }
+
+
 }

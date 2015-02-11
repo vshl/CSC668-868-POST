86086544e6c66fc0ffdb62fb31e6d45c92e8a134
Ryan Eshleman <ryaneshleman@gmail.com>
2015-02-10 17:20:49 -0800

    added some new methods to POST.java

--- src/POST.java
+++ src/POST.java
@@ -13,14 +13,15 @@ import java.util.List;
  * @author ryaneshleman
  */
 public class POST {
+
     private final static String PRODUCT_SPEC_FILE = "./products.txt";
     private final static String TRANSACTIONS_FILE = "./transactions.txt";
     private final static String SALES_LOG_FILE = "./sales_log.txt";
+    private static Catalog catalog;
+
     
-    private static Catalog    catalog;
     private List<Sale> sales = new ArrayList<Sale>();
-    
-    
+
     /**
      * @param args the command line arguments
      */
@@ -28,78 +29,94 @@ public class POST {
 
         POST post = new POST();
         post.init();
-        
-        
-        TransactionReader tr = new TransactionReader(post.getCatalog(),TRANSACTIONS_FILE);
-        
+
+
+        TransactionReader tr = new TransactionReader(post.getCatalog(), TRANSACTIONS_FILE);
+
         Sale s;
-        while(tr.hasNext())
-        {
+        while (tr.hasNext()) {
             s = tr.getNextSale();
-            System.out.println(s.generateInvoice());
-        }    
-        
+            printInvoice(s);
+
+        }
+
     }
     
+    private static void printInvoice(Sale s) {
+        System.out.println(s.generateInvoice());
+    }
+
     /**
-     * initializes the POST, builds 
+     * initializes the POST, builds
      */
-    public boolean init() throws IOException
-    {
+    public boolean init() throws IOException {
         catalog = new Catalog(PRODUCT_SPEC_FILE);
         return true;
         //throw new UnsupportedOperationException("Not yet implemented");
     }
-    
-    
-    public boolean shutDown()
-    {
+
+    public boolean shutDown() {
         throw new UnsupportedOperationException("Not yet implemented");
     }
-    
+
     /**
      * adds item to catalog
+     *
      * @param itemDesc
-     * @return 
+     * @return
      */
-    public ProductSpecification addToCatalog(ProductSpecification itemDesc)
-    {
-        throw new UnsupportedOperationException("Not yet implemented");
+    public ProductSpecification addToCatalog(ProductSpecification itemDesc) {
+        return catalog.addItem(itemDesc);
     }
-    
+
     /**
      * removes item from catalog
+     *
      * @param upc
-     * @return 
+     * @return
      */
-    public ProductSpecification removeFromCatalog(int upc)
-    {
+    public ProductSpecification removeFromCatalog(int upc) {
         throw new UnsupportedOperationException("Not yet implemented");
-    }  
-    
+    }
+
     /**
-     * if the sale is completed ie s.isCompleted == true, the sale is written
-     * in the sales log
+     * if the sale is completed ie s.isCompleted == true, the sale is written in
+     * the sales log
+     *
      * @param s
-     * @return 
+     * @return
      */
-    public boolean recordSale(Sale s)
-    {
+    public boolean recordSale(Sale s) {
         throw new UnsupportedOperationException("Not yet implemented");
     }
+
+  
     
-    /**
-     * returns the contents of the sales log 
-     * @return 
-     */
-    public String salesLog()
+    public Sale initiateSale(String custName, ArrayList<LineItem> lineItems)
     {
-        throw new UnsupportedOperationException("Not yet implemented");
+        Sale s = new Sale();
+        s.initiateSale(custName,lineItems);
+        return s;
     }
     
-    public Catalog getCatalog()
+    public Sale makePayment(Sale s, String paymentMethod, double amount, int cardNumber) throws Exception
     {
-        return catalog;
+        if(paymentMethod.equals("CASH"))
+            s.makeCashPayment(amount);
+        else if(paymentMethod.equals("CHECK"))
+            s.makeCheckPayment();
+        else 
+            s.makeCreditCardPayment(cardNumber);
+        return s;
+        
+            
     }        
     
+    public String salesLog() {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    public Catalog getCatalog() {
+        return catalog;
+    }
 }
